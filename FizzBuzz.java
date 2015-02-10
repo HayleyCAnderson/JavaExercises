aab478e0b9f0921386fb9b64ab6145360f984eee
Anderson <hand@USNYMACHAND.local>
2015-02-10 10:46:22 -0500

    Add FizzBuzz
    * First Java program!

---
+++ FizzBuzz.java
@@ -0,0 +1,21 @@
+public class FizzBuzz {
+  public static void main (String[] args) {
+    int lowerbound = 1;
+    int upperbound = 100;
+
+    for (int number = lowerbound; number <= upperbound; ++number) {
+      if ((number % 2) == 0) {
+        System.out.print("Fizz");
+      }
+      if ((number % 3) == 0) {
+        System.out.print("Buzz");
+      }
+      if (((number % 2) != 0) && ((number % 3) != 0)) {
+        System.out.print(number);
+      }
+
+      System.out.println();
+    }
+  }
+}
+
